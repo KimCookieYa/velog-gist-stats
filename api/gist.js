892b9a3497ef48b6a2a574293aa0a923b1c8beef
min49590 <min49590@gmail.com>
2023-04-13 02:28:59 +0900

    append .js

--- api/gist.js
+++ api/gist.js
@@ -1,4 +1,4 @@
-import { Octokit } from '@octokit/rest';
+import { Octokit } from '@octokit/rest.js';
 
 const createGist = async (lines) => {
     const octokit = new Octokit({ auth: `token ${process.env.GH_TOKEN}` });
