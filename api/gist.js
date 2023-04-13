4628f032cf2d30b3a1f14f5da5a10ee799339bf3
min49590 <min49590@gmail.com>
2023-04-13 11:07:58 +0900

    c

--- api/gist.js
+++ api/gist.js
@@ -1,4 +1,4 @@
-import { Octokit } from "https://cdn.skypack.dev/@octokit/rest";
+import { Octokit } from '@octokit/rest';
 
 const createGist = async (lines) => {
     const octokit = new Octokit({ auth: `token ${process.env.GH_TOKEN}` });
